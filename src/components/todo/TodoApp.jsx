5a720d8c477659e847f410377d79d119da60f976
Abhinav Jain <this.abhinav@gmail.com>
2024-03-13 11:38:03 +0530

    Changed look and feel

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -6,14 +6,17 @@ export default function TodoApp(){
     return (
         <div className="TodoApp">
             <BrowserRouter>
+                <HeaderComponent/>
                 <Routes>
                     <Route path='/' element={<LoginComponent />}/>
                     <Route path='/login' element={<LoginComponent />}/>
                     <Route path='/welcome/:username' element={<WelcomeComponent />}/>
                     <Route path='/todos' element={<ListTodosComponent />}/>
+                    <Route path = "/logout" element={<LogoutComponent/>}></Route>
                     <Route path='*' element={<ErrorComponent />}/>
                 </Routes>
             </BrowserRouter>
+            <FooterComponent/>
         </div>
     )
 }
@@ -108,9 +111,9 @@ function ListTodosComponent(){
     ];
     return(
         <div>
-            <div className="ListTodosComponent">
+            <div className="container">
                 <h1>Things you want to do</h1>
-                <table>
+                <table className="table">
                     <thead>
                         <tr>
                             <td>id</td>
@@ -134,6 +137,52 @@ function ListTodosComponent(){
                         }
                     </tbody>
                 </table>
+            </div>
+        </div>
+    )
+}
+
+function HeaderComponent(){
+    return(
+        <header className="border-bottom border-light border-5 mb-5 p-2">
+            <div className="container">
+                <div className="row">
+                    <nav className="navbar navbar-expand-lg">
+                        <Link className="navbar-brand ms-2 fs-2 fw-bold text-black" to="/">TODO App</Link>
+                        <div className="collapse navbar-collapse">
+                            <ul className="navbar-nav">
+                                <li className="nav-item fs-5"><Link className="nav-link" to="/welcome/admin">Home</Link></li>
+                                <li className="nav-item fs-5"><Link className="nav-link" to="/todos">Todos</Link></li>
+                            </ul>
+                        </div>
+                        <ul className="navbar-nav">
+                            <li className="nav-item fs-5"><Link className="nav-link" to="/login">Login</Link></li>
+                            <li className="nav-item fs-5"><Link className="nav-link" to="/logout">Logout</Link></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+function FooterComponent(){
+    return(
+        <div className="container">
+            <footer className="footer">
+                Footer
+            </footer>
+        </div>
+        
+    )
+}
+
+function LogoutComponent(){
+    return(
+        <div className="LogoutComponent">
+            <h1>You are logged out!!</h1>
+            <div>
+                Thank you for using our App. Come back soon!!
             </div>
         </div>
     )
